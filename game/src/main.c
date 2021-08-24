8cb198ad4b2c13bc9c02e04e75ac7a8b0cdcc30f
Anton <antonnrgrd@hotmail.com>
2021-08-24 22:41:33 +0200

    Fixed creatures slightly s.t they are properly drawn when becoming visisble

--- game/src/main.c
+++ game/src/main.c
@@ -38,13 +38,13 @@ int main(int argc, char *argv[]){
   initscr();
   start_color();    
   Game_World *game_world = g_generate_game_world(1000,1000);
-  //Creature *opponent = c_generate_creature(animal,0,63,14, game_world);
+  Creature *opponent = c_generate_creature(animal,0,63,14, game_world);
   Creature *c = c_random_player(63,2, game_world);
    Linked_List *ll = ll_initialize_linked_list();
-  // APPEND_NODE_CREATURE(ll,opponent);
+   APPEND_NODE_CREATURE(ll,opponent);
   Game_State *game_state = gs_create_game_state(c,game_world,ll);
-  //opponent->behavior = pursuing;
-  //opponent->target = c;
+  opponent->behavior = idle;
+  opponent->target = c;
   initscr();
   start_color();
   noecho();
